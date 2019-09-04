9eedb77bef0e1324cd4f2cefdd74dec6676309f0
Baraka <num46664@users.noreply.github.com>
2019-09-04 10:20:42 -0700

    source profile files, npm commands should work

--- project_installer.sh
+++ project_installer.sh
@@ -28,6 +28,9 @@ fi
 # TODO: waiting for asdf bugfix to handle yarn installs gracefully
 # npm i -g yarn@1.10.1;
 # yarn install;
+source ~/.bashrc;
+source ~/.bash_profile;
+
 npm i;
 
 npm ls -g --depth=0;
