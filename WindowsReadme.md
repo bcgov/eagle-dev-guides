b7809d9769df2f080776b8d8d7a470777c5a2ccd
Alastair Beaumont <a.beaumont11@gmail.com>
2019-10-16 13:20:44 -0700

    Quick changes into preq script
    Mostly added in my work into the WindowsReadme.md file. The script will need more work to fully work on windows. I added in all my hiccups into the windows readme as I installed the dependencies manually and used WSL.

---
+++ WindowsReadme.md
@@ -0,0 +1,26 @@
+# Windows installation notes
+
+## Must use Windows Subsystem for Linux (WSL)
+
+click [here](https://docs.microsoft.com/en-us/windows/wsl/install-win10) to install WSL
+
+## Installation
+
+- Run the install scripts on whichever repo you are on. Should work if running through wsl.
+  - If not update this document so future Windows users have an easier time!
+
+### Extra Notes / Known Problems
+
+- CHECK YOUR LINE ENDINGS! Use LF not CRLF! Make sure to set this up!
+  - This will be where most of your problems are coming from
+- Mongodb has problems with WSL. Check [here](https://github.com/Microsoft/WSL/issues/3286) for more information about that
+  - If running into problems I found [this](https://github.com/Microsoft/WSL/issues/3286#issuecomment-402594992) answer helped me
+  - Also [this](https://github.com/Microsoft/WSL/issues/796#issuecomment-255451470) helped me get it started up as a service
+- The functions don't seem to be working for me. So as of right now I have been installing the dependencies manually
+- Couldn't use asdf to install java openjdk 11 so just apt-install
+- Needed unzip to install gradle using asdf
+- Also make sure you set your line endings to lf. Click [here](https://help.github.com/en/articles/configuring-git-to-handle-line-endings) to see how to set that up in git
+
+### If not using wsl
+ - The scripts will not work without using wsl, you will need to install the dependecies manually
+ - Use chocolately and check the scripts for the proper versions
--- developer_install_prerequisites.sh
+++ developer_install_prerequisites.sh
@@ -6,15 +6,18 @@ PACKAGE_MANAGER="";
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     PACKAGE_MANAGER="brew";
+# Will be using the subsystem for now
 elif [[ "$OSTYPE" == "cygwin"* || "$OSTYPE" == "msys"* || "$OSTYPE" == "win"* ]]; then
     # POSIX compatibility layer and Linux environment emulation for Windows
     # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
     PACKAGE_MANAGER="choco";
 elif [[ "$OSTYPE" == "linux"* ]]; then
     if grep -qi Microsoft /proc/sys/kernel/osrelease 2> /dev/null; then
-        # Win10 bash
-        PACKAGE_MANAGER="choco";
+        # Need this for the installation of mongodb
+        WSL=true
+        PACKAGE_MANAGER="apt";
     else
+        # Win10 Subsystem will folow this path aswell
         # Linux
         DISTRO=$(lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om);
         if [[ "$DISTRO" == *"hat"* || "$DISTRO" == *"centos"* ]]; then
@@ -56,20 +59,42 @@ elif [[ "$PACKAGE_MANAGER" == "yum" ]]; then
     sudo yum -y install git code make;
 elif [[ "$PACKAGE_MANAGER" == "apt" ]]; then
     sudo apt-get update && sudo apt-get -y upgrade;
-    ccurl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg;
+     # This here is for vscode
+    sudo apt install software-properties-common apt-transport-https wget
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt-get -y install code
+
     sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/;
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
     sudo apt-get install apt-transport-https;
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10;
-    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list;
+    # Didn't seem to work for me
+    # echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list;
     sudo apt-get update;
-    sudo apt-get -y install build-essential git-core mongodb-org code;
+    sudo apt-get -y install build-essential git-core
+
+    if $WSL ; then
+      # This is to install mongodb for windows subsystem linux
+      # If this doesn't work follow the manual installation steps in the windows readme
+      wget https://www.mongodb.org/static/pgp/server-4.0.asc
+      sudo apt-key add server-4.0.asc
+      rm server-4.0.asc
+      sudo apt-get update
+      sudo apt-get install -y mongodb-org
+      curl -sL "https://www.mongodb.org/static/pgp/server-4.0.asc?_ga=2.264892495.1953852568.1531143056-750073170.1531143056" | sudo apt-key add
+    else
+      echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+      sudo apt-get update
+      sudo apt-get install -y mongodb-org
+    fi
 else
     echo -e \\n"Packages not installed.\\n"\\n
     exit 1;
 fi
 
 curl -LJO https://raw.githubusercontent.com/bcgov/eagle-dev-guides/master/vscodeextensions.txt;
+# This will loop the scripting service on windows. Comment this line out if you are running into a loop
 source ./vscodeextensions.txt;
 
 envProfileSettings "${PROFILE_FILE}";
